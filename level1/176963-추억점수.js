28d89c8cd11afccb0987e8b77fd13a1cd89005ae
eungbin <50731733+eungbin@users.noreply.github.com>
2023-10-18 16:57:07 +0900

    Update 176963-추억점수.js

--- level1/176963-추억점수.js
+++ level1/176963-추억점수.js
@@ -1,1 +1,18 @@
+// 1st solution
+function solution(name, yearning, photo) {
+    let answer = [];
+    
+    for(let i=0; i<photo.length; i++) {
+        let totalYearning = 0; // 현재 사진의 총 추억 점수
+        for(let j=0; j<photo[i].length; j++) {
+            let index = name.findIndex(e => e === photo[i][j]);
+            let nowYearning = index !== -1 ? yearning[index] : 0; // 현재 사진의 (j+1)번째 인물의 추억점수
+            totalYearning += nowYearning;
+        }
+        answer.push(totalYearning); // 현재 사진의 추억 점수 배열에 push
+    }
+    
+    return answer;
+}
 
+// 2nd solution
