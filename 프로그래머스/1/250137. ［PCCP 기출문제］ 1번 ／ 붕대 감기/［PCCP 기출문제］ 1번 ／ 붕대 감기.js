dd936fabc3c2336ae7e9b76dab41f5b1a631f2d2
eungbin <50731733+eungbin@users.noreply.github.com>
2024-04-05 13:48:23 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,16 +1,26 @@
 function solution(bandage, health, attacks) {
-    let maxHealth = health;
-    health -= attacks[0][1];
-    if(health <= 0) return -1;
-    
-    for(let i=1; i<attacks.length; i++) {
-        let healingTime = attacks[i][0] - attacks[i-1][0]-1;
-        let healingNum = (healingTime * bandage[1]) + (parseInt(healingTime / bandage[0]) * bandage[2]);
-        health = (health+healingNum > maxHealth ? maxHealth : health+healingNum);
-        
-        health -= attacks[i][1];
-        if(health <= 0) return -1;
+    const [useTime,hpPerSec,additional_hp,max_health] = [bandage[0],bandage[1],bandage[2],health]
+    let left_time, add_heal, heal,left_bandage_time,temp
+    if(attacks.length == 1){
+        return (health-attacks[0][1])<=0?-1:health-attacks[0][1]
     }
-    
-    return health;
+
+    for(let i=0; i<attacks.length-1; i++){
+        health-=attacks[i][1]  // 피 = 원래있던피 - 이번 공격
+        if(health<=0) return -1 //죽은 지 확인
+        left_time = attacks[i+1][0]-(attacks[i][0]+1)
+        
+        if(left_time<1) continue //남은 시간이 없으면 바로 다음 공격 맞으러 감
+        
+        if(left_time>=useTime){ //남은 시간이 시전 시간보다 길면 보너스
+            temp = parseInt(left_time/useTime)
+            heal = (temp*additional_hp) + (hpPerSec*left_time)
+        }else{
+             heal = left_time*hpPerSec
+        }
+        health+=heal
+        if(health>max_health) health = max_health
+    }
+    health -=attacks[attacks.length-1][1]
+    return health<=0? -1:health
 }
