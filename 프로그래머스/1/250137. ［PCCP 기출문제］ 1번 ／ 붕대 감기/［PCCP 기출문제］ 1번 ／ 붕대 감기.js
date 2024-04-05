12e9a2848e85695f88f354bed967b29876ad87ca
eungbin <50731733+eungbin@users.noreply.github.com>
2024-04-05 13:06:11 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,16 @@
+function solution(bandage, health, attacks) {
+    let maxHealth = health;
+    health -= attacks[0][1];
+    if(health <= 0) return -1;
+    
+    for(let i=1; i<attacks.length; i++) {
+        let healingTime = attacks[i][0] - attacks[i-1][0]-1;
+        let healingNum = (healingTime * bandage[1]) + (parseInt(healingTime / bandage[0]) * bandage[2]);
+        health = (health+healingNum > maxHealth ? maxHealth : health+healingNum);
+        
+        health -= attacks[i][1];
+        if(health <= 0) return -1;
+    }
+    
+    return health;
+}
